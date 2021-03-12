8971a1811f363be541d194941564bf25c194b2c9
Piotr Wątorski <piotr.watorski@gmail.com>
2021-03-12 23:25:45 +0100

    Minor tweaks

--- Screenoinator/MainForm.Designer.cs
+++ Screenoinator/MainForm.Designer.cs
@@ -116,7 +116,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(3, 316);
+            this.label9.Location = new System.Drawing.Point(3, 315);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(166, 221);
             this.label9.TabIndex = 28;
